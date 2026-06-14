899843f275cfcb617eeebf02f2773d9574fee236
Luqman Ibrahim <luqmanibrahim1995@gmail.com>
2026-06-14 12:57:51 +0800

    Improve Procedure Code V2

--- public/js/procedure.js
+++ public/js/procedure.js
@@ -1,17 +1,57 @@
-// procedure.js
-
 function loadProcedureList() {
 
     const centerPanel = document.querySelector('.center-panel');
 
     centerPanel.innerHTML = `
         <div class="procedure-page">
+
             <h2>📄 Senarai Prosedur</h2>
+
+            <button onclick="showAddProcedureForm()">
+                ➕ Tambah Prosedur
+            </button>
+
             <hr>
 
             <div class="procedure-container">
                 <p>Tiada prosedur lagi.</p>
             </div>
+
         </div>
     `;
+}
+
+function showAddProcedureForm() {
+
+    const centerPanel = document.querySelector('.center-panel');
+
+    centerPanel.innerHTML = `
+        <div class="procedure-page">
+
+            <h2>📄 Tambah Prosedur</h2>
+
+            <hr>
+
+            <label>Nama Prosedur</label><br>
+            <input type="text" id="procedureName"><br><br>
+
+            <label>Kod Prosedur</label><br>
+            <input type="text" id="procedureCode"><br><br>
+
+            <button onclick="saveProcedure()">
+                Simpan
+            </button>
+
+            <button onclick="loadProcedureList()">
+                Batal
+            </button>
+
+        </div>
+    `;
+}
+
+function saveProcedure() {
+
+    alert("Simpan prosedur belum dibuat lagi.");
+
 }
